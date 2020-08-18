0c94d659900cf08297aef3ce6721f7f69378902a
Max Lobur <max_lobur@outlook.com>
2020-08-18 18:57:02 +0300

    adjust to common helm var

--- kube_janitor/slack.py
+++ kube_janitor/slack.py
@@ -6,8 +6,9 @@ from slack.errors import SlackApiError
 class Slack:
     def __init__(self, config):
         self._config = config
-        self._client = WebClient(token=config['token'] or os.environ['SLACK_TOKEN'])
+        self._client = WebClient(os.environ['SLACK_TOKEN'])
         self._channel = config['channel']
+        self._cluster_name = os.environ['CLUSTER_NAME']
 
     def notify(self, message):
         self._post(message)
@@ -16,7 +17,10 @@ class Slack:
         try:
             response = self._client.chat_postMessage(
                 channel=self._channel,
-                text=msg)
+                text=msg,
+                as_user=False,
+                username="Kube Janitor of {}".format(self._cluster_name)
+            )
             assert response["message"]["text"] == msg
         except SlackApiError as e:
             assert e.response["ok"] is False
--- main.py
+++ main.py
@@ -12,15 +12,10 @@ slack_token = config['slack_token']
 def main():
     suggestions = kube_janitor.Janitor(kube, config).suggest()
 
-    kubectx = config['kube']['kubectx']
-    kubectl = config['kube']['kubectl']
-    context = config['kube']['context']
-    del_cmd = "\n".join(f"{kubectl} delete {k} {v}" for k, v in suggestions.items())
-
+    del_cmd = "\n".join(f"kubectl delete {k} {v}" for k, v in suggestions.items())
     message = f"""
-*{context}* I suggest to delete:
+I suggest to delete:
 ```
-{kubectx} {context}
 {del_cmd}
 ```
     """
