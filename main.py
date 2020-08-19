d8cb183c7d3fe00da0e0bfac33273ca666989ef0
Max Lobur <max_lobur@outlook.com>
2020-08-19 22:18:07 +0300

    load incluster config

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from confuse import Configuration
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-kubernetes.config.load_kube_config()
+try:
+    kubernetes.config.load_incluster_config()
+except kubernetes.config.config_exception.ConfigException:
+    kubernetes.config.load_kube_config()
+
 kube = kubernetes.client.CoreV1Api()
 config = Configuration('kube-janitor', __name__)
 slack_token = config['slack_token']
